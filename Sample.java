228f5ef953a0dc739c0bdd589da5f5052034bb62
Mr Omar <mukkaomar@gmail.com>
2025-01-03 19:38:41 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -9,4 +9,33 @@ public class Sample {
 
         // Display the result
         System.out.println("The result of multiplication is: " + result);}}
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout Code') {
+            steps {
+                git branch: 'main', url: 'https://github.com/Mromar1112/project1.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                
+                bat 'javac Sample.java'
+            }
+        }
+        stage('Test') {
+            steps {
+               
+                bat 'java Sample'
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo 'Deploying application...'
+                // Add your deployment commands here (like copying files to a server)
+            }
+        }
+    }
+}
 
